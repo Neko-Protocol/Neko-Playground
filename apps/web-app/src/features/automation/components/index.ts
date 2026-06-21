bb5138d805ffde1a526242ee9276125eda1db922
root <root@vmi3321457.contaboserver.net>
2026-06-21 11:20:22 +0200

    chore: separate analytics from automation PR

--- apps/web-app/src/features/automation/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as YieldRouter } from "./YieldRouter";
-export { useYieldRouter } from "../hooks/useYieldRouter";
-export type { YieldStrategy, RouterPosition, RouterStats, RouterEvent, YieldOpportunity, AllocationTarget, RiskProfile } from "../types";
--- apps/web-app/src/features/automation/index.ts
+++
@@ -1,3 +0,0 @@
-export { YieldRouter } from "./components";
-export { useYieldRouter } from "./hooks";
-export type { YieldStrategy, RouterPosition, RouterStats, RouterEvent, YieldOpportunity, AllocationTarget, RiskProfile } from "./types";
